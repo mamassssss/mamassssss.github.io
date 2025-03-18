f56a72a647f8fe1de5b6c306473aaaf31bdbd297
mamassssss <arasydimas4170@gmail.com>
2025-03-18 13:28:41 +0700

    sap

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
 <scrip><body></scrip> 
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
